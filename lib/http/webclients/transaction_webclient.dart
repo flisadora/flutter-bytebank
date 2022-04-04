01ae7ccde0b3011c4595578d06bd61319b5be9e6
Isadora <flisadora@gmail.com>
2022-04-04 12:53:29 +0100

    fixes + rebasing api db

--- lib/http/webclients/transaction_webclient.dart
+++ lib/http/webclients/transaction_webclient.dart
@@ -12,6 +12,12 @@ class TransactionWebClient {
     final List<dynamic> decodedJson = jsonDecode(response.body);
     return decodedJson.map((dynamic json) =>
         Transaction.fromJson(json)).toList();
+    /*List<Transaction> list = decodedJson.map((dynamic json) => Transaction.fromJson(json)).toList();
+    for(Transaction item in list){
+      delete(item.id);
+      print(item.toString());
+    }
+    return list;*/
     }
 
   Future<Transaction> save(Transaction transaction) async {
@@ -27,6 +33,19 @@ class TransactionWebClient {
     );
 
     return Transaction.fromJson(jsonDecode(response.body));
+  }
+
+  Future<Response> delete(int id) async {
+    final Response response = await client.delete(
+        Uri.parse('http://192.168.1.80:8080/transactions/$id'),
+        headers: {
+          'Content-type': 'application/json',
+          'password': '1000',
+        },
+    );
+    print('deleted');
+
+    return response;
   }
 
 }
